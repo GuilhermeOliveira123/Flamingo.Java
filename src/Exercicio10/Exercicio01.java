da35c7d9d8560a5e8ad477565327222e159ac126
Guilherme Oliveira <105466909+GuilhermeOliveira123@users.noreply.github.com>
2022-11-03 19:49:18 -0300

    Add files via upload

---
+++ src/Exercicio10/Exercicio01.java
@@ -0,0 +1,19 @@
+package exercicio10tarefaflamingo;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// 1) Apresentar os quadrados dos números inteiros de 15 a 200.
+		
+
+		int i, quad;// Declara as variáveis i (contador de looping) e quad(Que receberá os quadrados
+					// de i)
+		for (i = 15; i <= 200; i++) // contador para variar i de 15 até 200
+		{
+			quad = i * i; // quad recebe o valor de i ao quadrado
+			System.out.println("O quadrado de " + i + " é " + quad); // imprime os quadrados
+		}
+
+	}
+
+}
